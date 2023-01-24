c8f1887fbeeba79a037a5b73d830e19a6aeaea8a
jasaser <a11-3g@yandex.ru>
2023-01-24 22:33:22 +0300

    README file move to root

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[] {222,221,432,20,40});
+        SalesManager salesManager = new SalesManager(new int[]{222, 221, 432, 20, 40});
         int max = salesManager.max();
         System.out.println(max);
 
